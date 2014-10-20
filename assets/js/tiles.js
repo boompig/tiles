50f2de72814d5099291cd67f4ff0aa03c315fee8
Daniel Kats <dbkats@gmail.com>
2014-10-19 21:38:20 -0400

    scrolling and panning oh my

--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -1,7 +1,25 @@
 var canvas;
 var context;
 
+
+var max_radius = 5;
+var scrollSpeed = 1.03;
+
+var xOffset = 0;
+var yOffset = 0;
+var lastX = 0;
+var lastY = 0;
+var down = false;
+
 var HEX_SIZE = 50;
+
+function drawCircle (x, y, r, color) {
+    context.beginPath();
+    context.fillStyle = color;
+    context.arc(x, y, 0, Math.PI * 2, r, false);
+    context.stroke();
+    context.fill();
+}
 
 function drawHex(x_base, y_base, color) {
     context.save();
@@ -25,21 +43,23 @@ function drawHex(x_base, y_base, color) {
 
     context.stroke();
     context.fill();
+    //context.fillText("hello world", 0, 0);
+
     context.restore();
 }
 
-function drawRadial(q, r, color) {
+function drawRadial(q, r, color, options) {
     var x = HEX_SIZE * 3 / 2.0 * q;
     var y = HEX_SIZE * Math.sqrt(3) * (r + q / 2.0);
 
-    drawHex(x, y, color);
+    drawHex(x, y, color, options || {});
 }
 
 function drawRadialGrid() {
     context.save();
+    context.translate(xOffset, yOffset);
     context.translate(canvas.width / 2, canvas.height / 2);
 
-    var max_radius = 4;
     var colors = ["green", "orange", "purple", "aquamarine"];
 
     for (var q = -(max_radius-1); q < max_radius; q++) {
@@ -47,9 +67,11 @@ function drawRadialGrid() {
             if (q + r > (max_radius-1) || r+q < -(max_radius-1)){
                 continue;
             }
-            drawRadial(q, r, colors[getLayer(q, r) % colors.length]);
+            var layer = getLayer(q, r);
+            drawRadial(q, r, colors[layer % colors.length], {
+                label: layer
+            });
         }
-
     }
 
     context.restore();
@@ -65,24 +87,37 @@ function getLayer(q, r) {
 }
 
 function hoverHex(e) {
-    var x = e.pageX - canvas.offsetLeft - canvas.width / 2.0;
-    var y = e.pageY - canvas.offsetTop - canvas.height / 2.0;
+    var x = e.pageX - canvas.offsetLeft - canvas.width / 2.0 - xOffset;
+    var y = e.pageY - canvas.offsetTop - canvas.height / 2.0 - yOffset;
 
     //var across = Math.round( (x - canvas.width / 2.0) / HEX_SIZE );
     //var down = Math.round( (y - canvas.height / 2.0) / HEX_SIZE );
     var q = 2 / 3.0 * x / HEX_SIZE;
     var r = (-1/3.0 * x + 1/3.0 * Math.sqrt(3) * y) / HEX_SIZE;
 
+    if (down) {
+        var dx = e.clientX - lastX;
+        var dy = e.clientY - lastY;
+        xOffset += dx;
+        yOffset += dy;
+        lastX = e.clientX;
+        lastY = e.clientY;
+    }
 
-    canvas.width = canvas.width;
-    drawRadialGrid();
+    context.save();
+
+        canvas.width = canvas.width;
+        drawRadialGrid();
 
 
-    context.save()
-    context.translate(canvas.width / 2, canvas.height / 2);
-    drawRadial(Math.round(q), Math.round(r), "black");
+        if (! down) {
+            context.save();
+                context.translate(xOffset, yOffset);
+                context.translate(canvas.width / 2, canvas.height / 2);
+                drawRadial(Math.round(q), Math.round(r), "black");
+            context.restore();
+        }
     context.restore();
-    //console.log(across, down);
 }
 
 $(function() {
@@ -92,4 +127,31 @@ $(function() {
 
     drawRadialGrid();
     $(canvas).mousemove(hoverHex);
+    var scrollTop = 0;
+
+    $(window).mousedown(function (e) {
+        lastX = e.clientX;
+        lastY = e.clientY;
+        down = true;
+    });
+
+    $(window).mouseup(function (e) {
+        down = false;
+    });
+
+    $(window).scroll(function(e) {
+        e.preventDefault();
+        if ($(this).scrollTop() > scrollTop) {
+            // down, zoom out
+            HEX_SIZE /= scrollSpeed;
+        } else {
+            // up, zoom in
+            HEX_SIZE *= scrollSpeed;
+        }
+
+        canvas.width = canvas.width;
+        drawRadialGrid();
+
+        return false;
+    });
 });
